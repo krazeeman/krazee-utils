966c0d8a26aa665da4f00755c8530c4e3c01d846
krazee <krazee@qq.com>
2025-12-16 16:06:37 +0800

    添加map_utils

---
+++ map_utils.go
@@ -0,0 +1,19 @@
+package kzutils
+
+// MapGetKeys 获取map的所有key
+func MapGetKeys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// MapGetValues 获取map的所有value
+func MapGetValues[K comparable, V any](m map[K]V) []V {
+	values := make([]V, 0, len(m))
+	for _, v := range m {
+		values = append(values, v)
+	}
+	return values
+}
